74172837d8cc69c3d366c0ffd15ea99d6032c91b
hnagamin <1995.hnagamin@gmail.com>
2015-05-03 13:01:31 +0900

    replace grobal union-find tree with Struct UnionFindTree

--- data/union_find.cc
+++ data/union_find.cc
@@ -1,34 +1,39 @@
-const int NMAX = 100;
-int parent[NMAX], rank[NMAX];
+#include<vector>
+using namespace std;
 
-void init(int n) {
-  for (int i = 0; i < n; i++) {
-    par[i] = i;
-    rank[i] = 0;
+struct UnionFindTree {
+  vector<int> par, rank;
+  UnionFindTree(int n) {
+    par.reserve(n);
+    rank.reserve(n);
+    for (int i = 0; i < n; i++) {
+      par[i] = i;
+      rank[i] = 0;
+    }
   }
-}
 
-int find(int x) {
-  if (par[x] == x) {
-    return x;
-  } else {
-    return par[x] = find(par[x]);
+  int find(int i) {
+    if (par[i] == i) {
+      return i;
+    } else {
+      return par[i] = find(par[i]);
+    }
   }
-}
 
-void unite(int x, int y) {
-  x = find(x);
-  y = find(y);
-  if (x == y) return;
+  void unite(int i, int j) {
+    i = find(i);
+    j = find(j);
+    if (i == j) return;
 
-  if (rank[x] < rank[y]) {
-    par[x] = y;
-  } else {
-    par[y] = x;
-    if (rank[x] == rank[y]) rank[x]++;
+    if (rank[i] < rank[j]) {
+      par[i] = j;
+    } else {
+      par[j] = i;
+      if (rank[i] = rank[j]) rank[i]++;
+    }
   }
-}
 
-bool same(int x, int y) {
-  return find(x) == find(y);
-}
+  bool same(int i, int j) {
+    return find(i) == find(j);
+  }
+};
