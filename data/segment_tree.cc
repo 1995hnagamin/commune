8aee356a2e76a6b0ad63513b32df4461ce5ad0b1
Hideaki Nagamine <1995.hnagamin@gmail.com>
2015-06-18 08:55:47 +0900

    make segment tree template struct

--- data/segment_tree.cc
+++ data/segment_tree.cc
@@ -1,24 +1,17 @@
+// Verified AOJ DSL_2_A
 #include<vector>
 using namespace std;
 
-struct segment_tree {
-  typedef int T;
+template<typename T, T (*append)(T, T), T empty>
+struct Segtree {
   vector<T> dat;
   int n;
-  const T empty = 1 << 17;
-  T min(T x, T y) {
-    return (x < y ? x : y);
-  }
   
-  segment_tree(int n_) {
+  Segtree(int n_) {
     n = 1;
-    while (n < n_) {
-      n *= 2;
-    }
+    while (n < n_) n *= 2;
     dat.reserve(2 * n - 1);
-    for (int i = 0; i < 2 * n - 1; i++) {
-      dat[i] = empty;
-    }
+    for (int i = 0; i < 2 * n - 1; i++) dat[i] = empty;
   }
 
   void update(int k, T a) {
@@ -26,25 +19,26 @@ struct segment_tree {
     dat[k] = a;
     while (k > 0) {
       k = (k - 1) / 2;
-      dat[k] = min(dat[k * 2 + 1], dat[k * 2 + 2]);
+      dat[k] = append(dat[k * 2 + 1], dat[k * 2 + 2]);
     }
   }
 
   // min([a, b))
   T query(int a, int b, int k, int l, int r) {
-    if (r <= a || b <= l) {
-      return empty;
-    } else if (a <= l && r <= b) {
-      return dat[k];
-    } else {
+    if (r <= a || b <= l) return empty;
+    else if (a <= l && r <= b) return dat[k];
+    else {
       T vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
       T vr = query(a, b, k * 2 + 2, (l + r) / 2, r);
-      return min(vl, vr);
+      return append(vl, vr);
     }
   }
 
-  T query(int a, int b) {
-    return query(a, b, 0, 0, n);
-  }
+  T query(int a, int b) { return query(a, b, 0, 0, n); }
 };
+
+template<typename T>
+T getmin(T x, T y) {
+  return (x > y ? y : x);
+}
 
