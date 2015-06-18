58b02960a30d265ee9b7d8d1f6156242817193d2
Hideaki Nagamine <1995.hnagamin@gmail.com>
2015-06-18 21:01:55 +0900

    add Manacher's algorithm

---
+++ string/manacher.cc
@@ -0,0 +1,30 @@
+#include<vector>
+#include<iostream>
+#include<string>
+using namespace std;
+
+vector<int> manacher(string &S) {
+  int i = 0, j = 0, len = S.size();
+  vector<int> R(len, 0);
+  while (i < len) {
+    while (i-j >= 0 and i+j < len and S[i-j] == S[i+j]) ++j;
+    R[i] = j;
+    int k = 1;
+    while (i-k >= 0 and i+k < len and k+R[i-k] < j) {
+      R[i+k] = R[i-k];
+      ++k;
+    }
+    i+=k; j-=k;
+  }
+  return R;
+}
+
+int main() {
+  string s;
+  while (cin >> s) {
+    for (int x : manacher(s)) {
+      cout << x;
+    }
+    cout << endl;
+  }
+}
