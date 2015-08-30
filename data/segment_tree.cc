7bb718ddb2b3560f8742f85821a49e4f6a2f47d9
Hideaki Nagamine <1995hnagamin@users.noreply.github.com>
2015-08-31 02:31:42 +0900

    decide append function on runtime
    instead of using constexpr

--- data/segment_tree.cc
+++ data/segment_tree.cc
@@ -1,19 +1,21 @@
-// Verified AOJ DSL_2_A
-#include<vector>
-using namespace std;
-
-template<typename T, T (*append)(T, T)>
-struct Segtree {
+template<typename T>
+struct segment_tree {
   vector<T> dat;
   int n;
   T empty;
+  function<T(T,T)> append;
   
-  Segtree(int n_, T empty_) {
+  segment_tree(int n_, function<T(T,T)> append_, T empty_) {
     n = 1;
+    append = append_;
     empty = empty_;
-    while (n < n_) n *= 2;
+    while (n < n_) {
+      n *= 2;
+    }
     dat.reserve(2 * n - 1);
-    for (int i = 0; i < 2 * n - 1; i++) dat[i] = empty;
+    for (int i = 0; i < 2 * n - 1; i++) {
+      dat[i] = empty;
+    }
   }
 
   void update(int k, T a) {
@@ -25,22 +27,20 @@ struct Segtree {
     }
   }
 
-  // min([a, b))
+  // append([a, b))
   T query(int a, int b, int k, int l, int r) {
-    if (r <= a || b <= l) return empty;
-    else if (a <= l && r <= b) return dat[k];
-    else {
+    if (r <= a || b <= l) {
+      return empty;
+    } else if (a <= l && r <= b) {
+      return dat[k];
+    } else {
       T vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
       T vr = query(a, b, k * 2 + 2, (l + r) / 2, r);
       return append(vl, vr);
     }
   }
 
-  T query(int a, int b) { return query(a, b, 0, 0, n); }
+  T query(int a, int b) {
+    return query(a, b, 0, 0, n);
+  }
 };
-
-template<typename T>
-T getmin(T x, T y) {
-  return (x > y ? y : x);
-}
-
