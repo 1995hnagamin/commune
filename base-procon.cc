fa1cfa6de06855694ac0b60f84bf708466f74f70
Hideaki Nagamine <1995.hnagamin@gmail.com>
2015-09-24 22:12:07 +0900

    add base-procon.cc

---
+++ base-procon.cc
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define FOR(i,a,b) for (int i=(a);i<(b);i++)
+#define RFOR(i,a,b) for (int i=(b)-1;i>=(a);i--)
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define RREP(i,n) for (int i=(n)-1;i>=0;i--)
+#define ALL(a) (a).begin(),(a).end()
+const int INF = INT_MAX/3;
+const double EPS = 1e-14;
+
+template<typename U, typename T>
+void chmin(U &x, T y) { x = min(x, y); }
+
+template<typename U, typename T>
+void chmax(U &x, T y) { x = max(x, y); }
+
+typedef long long int lli;
+typedef long double ld;
+
+{{_cursor_}}
+
+int main() {
+  cout << fixed << setprecision(10);
+
+  while () {
+  }
+  return 0;
+}
