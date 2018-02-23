c114e98b363c20b8bb23c3467749f5b7ab6ddd6a
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2018-02-24 04:02:01 +0900

    Remove install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-find . -name *.cc | while read File
-do
-  fname=`basename $File`
-  name="${fname%.*}"
-  echo $name
-  cp ${File} ${HOME}/.vim/bundle/sonictemplate-vim/template/cpp/${name}.cpp
-done
-
