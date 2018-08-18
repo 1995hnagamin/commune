8e5dc1ef902eee2125822be926bab39244b42ed9
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2018-08-18 22:45:54 +0900

    Don't use global variables (ford-fulkerson.cpp)

--- graph/ford-fulkerson.cpp
+++ graph/ford-fulkerson.cpp
@@ -1,40 +1,43 @@
-const int VMAX;
+class FordFulkerson {
+  public:
+    struct edge { int to, cap, rev; };
 
-struct edge { int to, cap, rev; };
+    explicit FordFulkerson(int v): G(v), used(v) {}
+    ~FordFulkerson() = default;
 
-vector<edge> G[VMAX];
-bool used[VMAX];
+    void add_edge(int from, int to, int cap) {
+      G[from].push_back({to, cap, (int)G[to].size()});
+      G[to].push_back({from, 0, (int)G[from].size()-1});
+    }
+    int dfs(int v, int t, int f) {
+      if (v == t) return f;
+      used[v] = true;
 
-void add_edge(int from, int to, int cap) {
-  G[from].push_back((edge){to, cap, (int)G[to].size()});
-  G[to].push_back((edge){from, 0, (int)G[from].size() - 1});
-}
+      for (int i = 0; i < G[v].size(); i++) {
+        edge &e = G[v][i];
+        if (!used[e.to] and e.cap > 0) {
+          int d = dfs(e.to, t, std::min(f, e.cap));
+          if (d > 0) {
+            e.cap -= d;
+            G[e.to][e.rev].cap += d;
+            return d;
+          }
+        }
+      }
+      return 0;
+    }
+    int max_flow(int s, int t) {
+      int flow = 0;
 
-int dfs(int v, int t, int f) {
-  if (v == t) return f;
-  used[v] = true;
-
-  for (int i = 0; i < G[v].size(); i++) {
-    edge &e = G[v][i];
-    if (!used[e.to] and e.cap > 0) {
-      int d = dfs(e.to, t, min(f, e.cap));
-      if (d > 0) {
-        e.cap -= d;
-        G[e.to][e.rev].cap += d;
-        return d;
+      while (true) {
+        std::fill(used.begin(), used.end(), 0);
+        int f = dfs(s, t, INF);
+        if (f == 0) return flow;
+        flow += f;
       }
     }
-  }
-  return 0;
-}
 
-int max_flow(int s, int t) {
-  int flow = 0;
-
-  while (true) {
-    memset(used, 0, sizeof(used));
-    int f = dfs(s, t, INF);
-    if (f == 0) return flow;
-    flow += f;
-  }
-}
+  private:
+    std::vector<std::vector<edge>> G;
+    std::vector<bool> used;
+};
