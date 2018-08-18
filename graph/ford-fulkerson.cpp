df0948b25f4fccafccdc1b2baf481dc92277e9a4
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2018-08-19 01:36:57 +0900

    Minor updates to graph/ford-fulkerson.cpp
    Add constructor to FordFulkerson::edge.
    Use std::vector::emplace_back.
    Use size_t for vector indices.
    Add FordFulkerson::reverse(edge const &).

--- graph/ford-fulkerson.cpp
+++ graph/ford-fulkerson.cpp
@@ -1,34 +1,33 @@
 class FordFulkerson {
   public:
-    struct edge { int to, cap, rev; };
+    struct edge {
+      explicit edge(size_t t, int c, size_t r) : to(t), cap(c), rev(r) {}
+      size_t to; int cap; size_t rev;
+    };
 
-    explicit FordFulkerson(int v): G(v), used(v) {}
+    explicit FordFulkerson(size_t v): G(v), used(v) {}
     ~FordFulkerson() = default;
 
-    void add_edge(int from, int to, int cap) {
-      G[from].push_back({to, cap, (int)G[to].size()});
-      G[to].push_back({from, 0, (int)G[from].size()-1});
+    void add_edge(size_t from, size_t to, int cap) {
+      G[from].emplace_back(to, cap, G[to].size());
+      G[to].emplace_back(from, 0, G[from].size() - 1);
     }
-    int dfs(int v, int t, int f) {
+    int dfs(size_t v, size_t t, int f) {
       if (v == t) return f;
       used[v] = true;
-
-      for (int i = 0; i < G[v].size(); i++) {
-        edge &e = G[v][i];
-        if (!used[e.to] and e.cap > 0) {
-          int d = dfs(e.to, t, std::min(f, e.cap));
-          if (d > 0) {
-            e.cap -= d;
-            G[e.to][e.rev].cap += d;
-            return d;
-          }
+      for (auto &&e : G[v]) {
+        if (used[e.to] || e.cap <= 0) continue;
+        int d = dfs(e.to, t, std::min(f, e.cap));
+        if (d > 0) {
+          e.cap -= d;
+          reverse(e).cap += d;
+          return d;
         }
       }
       return 0;
     }
-    int max_flow(int s, int t) {
+    int max_flow(size_t s, size_t t) {
       int flow = 0;
-
       while (true) {
         std::fill(used.begin(), used.end(), 0);
         int f = dfs(s, t, INF);
@@ -40,4 +39,7 @@ class FordFulkerson {
   private:
     std::vector<std::vector<edge>> G;
     std::vector<bool> used;
+    edge &reverse(edge const &e) {
+      return G[e.to][e.rev];
+    }
 };
