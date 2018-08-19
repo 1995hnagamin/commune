a16f30ce9389845cc96f90c6a4d06264332d1c1c
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2018-08-19 17:15:19 +0900

    Minor updates to data/union-find.cpp
    Reformat.
    Use size_t.
    Add explicit specifier to the constructor declaration.

--- data/union-find.cpp
+++ data/union-find.cpp
@@ -1,36 +1,36 @@
-struct UnionFindTree {
-  vector<int> par, rank;
-  UnionFindTree(int n) {
-    par.reserve(n);
-    rank.reserve(n);
-    for (int i = 0; i < n; i++) {
-      par[i] = i;
-      rank[i] = 0;
+class UnionFindTree {
+  public:
+    explicit UnionFindTree(size_t n): par(n), rank(n, 0) {
+      for (size_t i = 0; i < n; ++i) {
+        par[i] = i;
+      }
     }
-  }
 
-  int find(int i) {
-    if (par[i] == i) {
-      return i;
-    } else {
-      return par[i] = find(par[i]);
+    size_t find(size_t i) {
+      if (par[i] == i) {
+        return i;
+      } else {
+        return par[i] = find(par[i]);
+      }
     }
-  }
 
-  void unite(int i, int j) {
-    i = find(i);
-    j = find(j);
-    if (i == j) return;
+    void unite(size_t i, size_t j) {
+      i = find(i);
+      j = find(j);
+      if (i == j) return;
 
-    if (rank[i] < rank[j]) {
-      par[i] = j;
-    } else {
-      par[j] = i;
-      if (rank[i] = rank[j]) rank[i]++;
+      if (rank[i] < rank[j]) {
+        par[i] = j;
+      } else {
+        par[j] = i;
+        if (rank[i] = rank[j]) rank[i]++;
+      }
     }
-  }
 
-  bool same(int i, int j) {
-    return find(i) == find(j);
-  }
+    bool same(size_t i, size_t j) {
+      return find(i) == find(j);
+    }
+  private:
+    std::vector<size_t> par;
+    std::vector<size_t> rank;
 };
