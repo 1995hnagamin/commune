8be19da419165959e551f90c399caf74826da010
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2018-09-30 02:06:52 +0900

    Rewrite thrifty-segment-tree.cpp

--- data/thrifty-segment-tree.cpp
+++ data/thrifty-segment-tree.cpp
@@ -1,59 +1,74 @@
-// Verified AOJ DSL_2_A
-template<typename T, T(*append)(T,T)>
-struct Segtree {
-  struct Node {
-    int lp, rp; // [l, r)
-    T v;
-    Node *lc, *rc;
-    Node(T v_, int lp_, int rp_) 
-      : v(v_), lp(lp_), rp(rp_), lc(NULL), rc(NULL) {}
-  };
-  Node* root;
-  T empty;
-  
-  T minv(Node* x, Node* y) {
-    if (!x) return (!y)?empty:y->v;
-    if (!y) return x->v;
-    return append(x->v, y->v);
-  }
-  
-  void update(Node* t, int k, T a) {
-    if (t->rp - t->lp <= 1 and t->lp == k) {
-      t->v = a;
-      return;
-    } 
-    int mid = (t->lp + t->rp) / 2;
-    if (k < mid) {
-      if (!(t->lc)) t->lc = new Node(empty, t->lp, mid);
-      update(t->lc, k, a);
-    } else {
-      if (!(t->rc)) t->rc = new Node(empty, mid, t->rp);
-      update(t->rc, k, a);
+template<typename T, typename FnT, typename IndexT = int>
+class ThriftySegmentTree {
+  public:
+    struct Node {
+      T v;
+      IndexT lp, rp; // [l, r)
+      std::unique_ptr<Node> lc, rc;
+      Node(T v_, IndexT lp_, IndexT rp_):
+        v(v_), lp(lp_), rp(rp_), lc(), rc() {}
+      void update(FnT const &append, T const &unit, IndexT k, T x) {
+        if (rp <= lp + 1 && lp == k) {
+          v = x;
+          return;
+        }
+        const auto mid = (lp + rp) / 2;
+        if (k < mid) {
+          if (!lc) lc = std::make_unique<Node>(unit, lp, mid);
+          lc->update(append, unit, k, x);
+        } else {
+          if (!rc) rc = std::make_unique<Node>(unit, mid, rp);
+          rc->update(append, unit, k, x);
+        }
+        v = m_append(append, unit);
+      }
+      T m_append(FnT const &append, T const &unit) const {
+        if (!lc) { return rc? rc->v : unit; }
+        if (!rc) { return lc->v; }
+        return append(lc->v, rc->v);
+      }
+      T query(FnT const &append, T const &unit, IndexT a, IndexT b) const {
+        if (b <= lp || rp <= a) { return unit; }
+        if (a <= lp && rp <= b) { return v; }
+        const auto vl = lc? lc->query(append, unit, a, b) : unit;
+        const auto vr = rc? rc->query(append, unit, a, b) : unit;
+        return append(vl, vr);
+      }
+      T at(T const &unit, IndexT k) const {
+        const auto mid = (lp + rp) / 2;
+        if (k < mid) {
+          return lc? lc->at(unit, k) : unit;
+        } else {
+          return rc? rc->at(unit, k) : unit;
+        }
+      }
+    };
+    explicit ThriftySegmentTree(FnT const &app, T const &un, IndexT n):
+      append(app), unit(un), N(1 << log2(n)),
+      root(std::make_unique<Node>(un, 0, N)) {
     }
-    t->v = minv(t->lc, t->rc);
-    return;
-  }
-  void update(int k, T a) { update(root, k, a); }
-  
-  // min([a,b))
-  T query(Node* t, int a, int b) {
-    if (!t) return empty;
-    if (t->rp <= a || b <= t->lp) return empty;
-    else if (a <= t->lp && t->rp <= b) return t->v;
-    else {
-      T vl = query(t->lc, a, b), vr = query(t->rc, a, b);
-      return append(vl, vr);
+    ~ThriftySegmentTree() = default;
+    void update(IndexT k, T x) {
+      root->update(append, unit, k, x);
     }
-  }
-  T query(int a, int b) { return query(root, a, b); }
+    T at(IndexT k) const {
+      return root->at(unit, k);
+    }
+    T query(IndexT a, IndexT b) const {
+      return root->query(append, unit, a, b);
+    }
+    T query() const {
+      return query(0, N);
+    }
+  private:
+    FnT const append;
+    T const unit;
+    size_t const N;
+    std::unique_ptr<Node> root;
 
-  Segtree(int n_, T empty_) {
-    int n = 1;
-    empty = empty_;
-    while (n < n_) n*=2;
-    root = new Node(empty, 0, n);
-  }
+    static size_t log2(size_t n) {
+      size_t p = 1;
+      while ((1 << p) < n) { ++p; }
+      return p;
+    }
 };
-
-template<typename T>
-T getmin(T a, T b) { return (a > b ? b : a); }
