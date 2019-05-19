b1e81bddd930e0197aaa8707b59ccca56705670b
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-05-20 03:19:29 +0900

    Add graph/dijkstra.cpp

---
+++ graph/dijkstra.cpp
@@ -0,0 +1,33 @@
+namespace { // Dijkstra's algorithm
+
+template<typename WeightT>
+struct DijkstraResult {
+  std::vector<WeightT> distance;
+};
+
+template<typename WeightT>
+DijkstraResult<WeightT>
+dijkstra_algorithm(AdjacencyList<WeightT> const &g, int start, WeightT inf) {
+  int const N = g.size();
+  std::vector<bool> done(N, false);
+  std::vector<WeightT> dist(N, inf);
+  using P = std::tuple<WeightT, int>;
+  std::priority_queue<P, std::vector<P>, std::greater<P> > que;
+  que.emplace(0, start);
+  while (!que.empty()) {
+    WeightT dist2u; int u; std::tie(dist2u, u) = que.top();
+    que.pop();
+    if (dist2u >= dist[u]) {
+      continue;
+    }
+    dist[u] = dist2u;
+    for (auto &&e : g[u]) {
+      que.emplace(dist2u + e.weight, e.to);
+    }
+  }
+  return {
+    dist
+  };
+}
+
+} // namespace
