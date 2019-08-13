8772047030de241b9b90b2e58f3dbfaf033d8835
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-08-14 00:32:54 +0900

    Set base(s) of rolling hash at runtime

--- string/rolling-hash.cpp
+++ string/rolling-hash.cpp
@@ -1,38 +1,42 @@
-namespace { // RollingHash
-
-template<typename T, typename I, I... base>
+template<typename T, size_t L, typename I = T>
 class RollingHash {
   public:
-    size_t static constexpr L = sizeof...(base);
-    std::array<T, L> static constexpr p = { T(base)... };
-
-    explicit RollingHash(std::string const &s_): s(s_), n(s_.size()), pow(), phash() {
-      REP(z, L) {
+    explicit RollingHash(std::string const &s, std::array<I, L> const &base):
+      p(convert(base)), str(s), n(str.size()), pow(), phash() {
+      for (size_t z = 0; z < L; ++z) {
         pow[z] = std::vector<T>(n+1);
         phash[z] = std::vector<T>(n+1);
         pow[z][0] = 1, phash[z][0] = 0;
       }
-      REP(z, L) REP(i, n) {
-        phash[z][i+1] = T(s[i]) + phash[z][i] * p[z];
+      for (size_t z = 0; z < L; ++z) for (size_t i = 0; i < n; ++i) {
+        phash[z][i+1] = T(str[i]) + phash[z][i] * p[z];
         pow[z][i+1] = pow[z][i] * p[z];
       }
     }
-    std::array<T, L> hash_substr(size_t i, size_t j) const {
+    // [i, j)
+    std::array<T, L> hash_range(size_t i, size_t j) const {
       std::array<T, L> ret;
-      REP(z, L) {
+      for (size_t z = 0; z < L; ++z) {
         ret[z] = phash[z][j] - phash[z][i] * pow[z][j-i];
       }
       return ret;
     }
 
   private:
-    std::string s;
-    int n;
+    static std::array<T, L> convert(std::array<I, L> const &v) {
+      auto const n = v.size();
+      std::array<T, L> w;
+      for (size_t i = 0; i < n; ++i) {
+        w[i] = static_cast<T>(v[i]);
+      }
+      return w;
+    }
+
+  public:
+    std::array<T, L> const p;
+  private:
+    std::string str;
+    size_t n;
     std::array<std::vector<T>, L> pow;
     std::array<std::vector<T>, L> phash;
 };
-
-template<typename T, typename I, I... base>
-std::array<T, RollingHash<T, I, base...>::L> const RollingHash<T, I, base...>::p;
-
-} // namespace
