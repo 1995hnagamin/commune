496323d3805a9ff4646667615974d6290f21d8f0
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-08-22 21:36:39 +0900

    Minor updates to manacher.cpp

--- string/manacher.cpp
+++ string/manacher.cpp
@@ -4,14 +4,14 @@ manacher(std::string const &S) {
   const auto len = static_cast<size_t>(S.size());
   std::vector<size_t> R(len, 0);
   while (i < len) {
-    while (i >= j && i+j < len && S[i-j] == S[i+j]) ++j;
+    while (i >= j && i+j < len && S[i-j] == S[i+j]) { ++j; }
     R[i] = j;
     size_t k = 1;
     while (i >= k && i+k < len && k+R[i-k] < j) {
       R[i+k] = R[i-k];
       ++k;
     }
-    i+=k; j-=k;
+    i += k; j -= k;
   }
   return R;
 }
