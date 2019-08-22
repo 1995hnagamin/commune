f60d21e78f095f237a6d4803caa534d969009e1e
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-08-22 21:07:09 +0900

    Use snake_case for class name (sunzi.cpp)

--- math/sunzi.cpp
+++ math/sunzi.cpp
@@ -1,30 +1,30 @@
-namespace { // Sunzi
+namespace { // sunzi
 
 template <uint32_t... m>
-class Sunzi {
+class sunzi {
   public:
     constexpr static size_t L = sizeof...(m);
     using I = uint_fast32_t;
 
-    constexpr Sunzi(): dat() { dat.fill(0); }
-    constexpr Sunzi(int n): dat({ (n >= 0 ? n%m : m-((-n)%m))... }) { }
-    constexpr Sunzi(Sunzi const &other) = default;
+    constexpr sunzi(): dat() { dat.fill(0); }
+    constexpr sunzi(int n): dat({ (n >= 0 ? n%m : m-((-n)%m))... }) { }
+    constexpr sunzi(sunzi const &other) = default;
     constexpr std::array<I, L> data() const { return dat; }
-    constexpr Sunzi &operator+=(Sunzi const &other) {
+    constexpr sunzi &operator+=(sunzi const &other) {
       constexpr std::array<I,L> mod = { m... };
       for (size_t i = 0; i < L; ++i) {
         dat[i] = (dat[i] + other.dat[i]) % mod[i];
       }
       return *this;
     }
-    constexpr Sunzi &operator-=(Sunzi const &other) {
+    constexpr sunzi &operator-=(sunzi const &other) {
       constexpr std::array<I,L> mod = { m... };
       for (size_t i = 0; i < L; ++i) {
         dat[i] = (dat[i] + mod[i] - other.dat[i]) % mod[i];
       }
       return *this;
     }
-    constexpr Sunzi &operator*=(Sunzi const &other) {
+    constexpr sunzi &operator*=(sunzi const &other) {
       constexpr std::array<I,L> mod = { m... };
       for (size_t i = 0; i < L; ++i) {
         uint_fast64_t n = ((uint_fast64_t)dat[i]) * ((uint_fast64_t)other.dat[i]);
@@ -38,26 +38,26 @@ class Sunzi {
 };
 
 template<uint32_t... m>
-constexpr bool operator==(Sunzi<m...> const &a, Sunzi<m...> const &b) {
+constexpr bool operator==(sunzi<m...> const &a, sunzi<m...> const &b) {
   return a.data() == b.data();
 }
 
 template<uint32_t... m>
-constexpr Sunzi<m...> const operator+(Sunzi<m...> const &a, Sunzi<m...> const &b) {
+constexpr sunzi<m...> const operator+(sunzi<m...> const &a, sunzi<m...> const &b) {
   auto num = a;
   num += b;
   return num;
 }
 
 template<uint32_t... m>
-constexpr Sunzi<m...> const operator-(Sunzi<m...> const &a, Sunzi<m...> const &b) {
+constexpr sunzi<m...> const operator-(sunzi<m...> const &a, sunzi<m...> const &b) {
   auto num = a;
   num -= b;
   return num;
 }
 
 template<uint32_t... m>
-constexpr Sunzi<m...> const operator*(Sunzi<m...> const &a, Sunzi<m...> const &b) {
+constexpr sunzi<m...> const operator*(sunzi<m...> const &a, sunzi<m...> const &b) {
   auto num = a;
   num *= b;
   return num;
