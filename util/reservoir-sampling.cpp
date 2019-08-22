188af79707f539056f46e8d13b3211a6a5d39db4
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-08-22 16:26:34 +0900

    Minor updates to reservoir_sampling()

--- util/reservoir-sampling.cpp
+++ util/reservoir-sampling.cpp
@@ -2,7 +2,7 @@ template<typename T, typename Iterator>
 std::vector<T> reservoir_sampling(Iterator begin, Iterator end, int k) {
   std::vector<T> ret(k);
   Iterator iter = begin; int n;
-  for (n = 0; n < k; ++n) { ret[n] = *iter; ++iter; }
+  for (n = 0; n < k; ++n, ++iter) { ret[n] = *iter; }
   std::random_device rd; std::mt19937 g(rd());
   for (; iter != end; ++iter, ++n) {
     std::uniform_int_distribution<> dis(0, n-1);
