f1b04d938cd58b98032e24c8ae12eafd614d0243
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-08-22 18:57:41 +0900

    Rewrite persistent_segment_tree

--- data/persistent-segment-tree.cpp
+++ data/persistent-segment-tree.cpp
@@ -1,68 +1,81 @@
-template<typename T, T(*append)(T,T)>
+namespace { // persistent segment tree
+
+template<typename T, typename FnT>
 class persistent_segment_tree {
   public:
     struct Node {
       int lp, rp; // [l, r)
       T v;
-      Node *lc, *rc;
-      Node(T v_, int lp_, int rp_) :
-        v(v_), lp(lp_), rp(rp_), lc(), rc() {}
-      Node() : lc(), rc() {}
+      std::shared_ptr<Node> lc, rc;
+      Node(T v_, int lp_, int rp_): lp(lp_), rp(rp_), v(v_), lc(), rc() {
+      }
+      T m_append(FnT const &append, T const &unit) const {
+        if (!lc) { return rc? rc->v : unit; }
+        if (!rc) { return lc->v; }
+        return append(lc->v, rc->v);
+      }
+      std::shared_ptr<Node>
+      update(FnT const &append, T const &unit, int k, T const &a) const {
+        if (rp - lp <= 1 && lp == k) {
+          auto const t = std::make_shared<Node>(a, lp, rp);
+          return t;
+        }
+        auto t = std::make_shared<Node>(*this);
+        int mid = (lp + rp) / 2;
+        if (k < mid) {
+          auto const ch = lc? lc : std::make_shared<Node>(unit, lp, mid);
+          t->lc = ch->update(append, unit, k, a);
+        } else {
+          auto const ch = rc? rc : std::make_shared<Node>(unit, mid, rp);
+          t->rc = ch->update(append, unit, k, a);
+        }
+        t->v = t->m_append(append, unit);
+        return t;
+      }
+      T query(FnT const &append, T const &unit, int a, int b) const {
+        if (b <= lp || rp <= a) { return unit; }
+        if (a <= lp && rp <= b) { return v; }
+        auto const vl = lc? lc->query(append, unit, a, b) : unit;
+        auto const vr = rc? rc->query(append, unit, a, b) : unit;
+        return append(vl, vr);
+      }
     };
-    vector<Node*> roots;
-    T empty;
 
-    T minv(Node* x, Node* y) {
-      if (!x) return (!y)?empty:y->v;
-      if (!y) return x->v;
-      return append(x->v, y->v);
-    }
-
-    Node* update(const Node* t, int k, T a) {
-      if (t->rp - t->lp <= 1 and t->lp == k) {
-        Node* t2 = new Node(a, t->lp, t->rp);
-        return t2;
-      }
-      Node* t2 = t;
-      int mid = (t->lp + t->rp) / 2;
-      if (k < mid) {
-        if (!(t->lc)) t2->lc = new Node(empty, t->lp, mid);
-        t2->lc = update(t->lc, k, a);
-      } else {
-        if (!(t->rc)) t->rc = new Node(empty, mid, t->rp);
-        t2->rc = update(t->rc, k, a);
-      }
-      t2->v = minv(t2->lc, t2->rc);
-      return t2;
-    }
-    int update(int k, T a) {
-      Node* t = update(roots.back(), k, a);
+    size_t update(int k, T a) {
+     auto const t = roots.back()->update(append, unit, k, a);
       roots.push_back(t);
       return roots.size() - 1;
     }
 
-  // min([a,b))
-    T query(Node* t, int a, int b) {
-      if (!t) return empty;
-      if (t->rp <= a || b <= t->lp) return empty;
-      else if (a <= t->lp && t->rp <= b) return t->v;
-      else {
-        T vl = query(t->lc, a, b), vr = query(t->rc, a, b);
-        return append(vl, vr);
-      }
-    }
+    // min([a,b))
     T query(int idx, int a, int b) {
-      return query(roots[idx], a, b);
+      return roots[idx]->query(append, unit, a, b);
     }
 
-    persistent_segment_tree(int n_, T empty_) {
-      int n = 1;
-      empty = empty_;
-      while (n < n_) n*=2;
-      Node* t2 = new Node(empty, 0, n);
-      roots.push_back(t2);
+    explicit persistent_segment_tree(FnT const &app, T const &un, int n):
+      append(app), unit(un), N(1 << log2(n)), roots()
+    {
+      roots.emplace_back(std::make_shared<Node>(unit, 0, N));
     }
+
+    static size_t log2(size_t n) {
+      size_t p = 1;
+      while ((1 << p) < n) { ++p; }
+      return p;
+    }
+
+  private:
+    FnT const append;
+    T const unit;
+    size_t const N;
+    std::vector<std::shared_ptr<Node>> roots;
 };
 
-template<typename T>
-T getmin(T a, T b) { return (a > b ? b : a); }
+template<typename T, typename FnT>
+persistent_segment_tree<T, typename std::decay<FnT>::type>
+make_persistent_segment_tree(FnT const &app, T const &un, int n) {
+  using F = typename std::decay<FnT>::type;
+  return persistent_segment_tree<T, F>(app, un, n);
+}
+
+} // namespace
