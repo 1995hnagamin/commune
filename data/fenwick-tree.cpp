efa003364586f1c0fc857374b074cd05f27611bf
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-08-22 20:56:56 +0900

    Use size_t for indices (fenwick-tree.cpp)

--- data/fenwick-tree.cpp
+++ data/fenwick-tree.cpp
@@ -1,31 +1,28 @@
 template<typename T>
 class fenwick_tree {
   public:
-    fenwick_tree(int size, T z): N(size), zero(z), bit(size+1, z) {
+    fenwick_tree(size_t size, T z): N(size), zero(z), bit(size+1, z) {
     }
 
     // [1,k]
-    T sum(int k) const {
+    T sum(size_t k) const {
       T s = zero;
-      for (int i = k; i > 0; i -= i&(-i)) {
-        s += bit[i];
-      }
+      for (size_t i = k; i > 0; i -= i&(-i)) { s += bit[i]; }
       return s;
     }
 
     // [l,r)
-    T sum(int l, int r) const {
+    T sum(size_t l, size_t r) const {
+      assert(0 < l && l <= r);
       return sum(r-1) - sum(l-1);
     }
 
     void add(int k, T x) {
-      for (int i = k; i <= N; i += i&(-i)) {
-        bit[i] += x;
-      }
+      for (size_t i = k; i <= N; i += i&(-i)) { bit[i] += x; }
     }
 
   private:
-    int const N;
+    size_t const N;
     T const zero;
     std::vector<T> bit;
 };
