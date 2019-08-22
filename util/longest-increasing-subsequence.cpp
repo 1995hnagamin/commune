2346d4d34b439be270265184625a5bc048039597
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-08-22 21:25:19 +0900

    Prefer functions to classes

--- util/longest-increasing-subsequence.cpp
+++ util/longest-increasing-subsequence.cpp
@@ -1,26 +1,23 @@
-template<typename T, T inf>
-class longest_incr_seq {
-  public:
-    longest_incr_seq(std::vector<T> const &s):
-      n(s.size()), seq(s), dp(n, inf) {}
-    size_t solve() {
-      for (size_t i = 0; i < n; ++i) {
-        *std::lower_bound(dp.begin(), dp.end(), seq[i]) = seq[i];
-      }
-      return std::distance(
-          dp.begin(),
-          std::lower_bound(dp.begin(), dp.end(), inf));
-    }
-    size_t solve(bool (*comp)(T const &, T const &)) {
-      for (size_t i = 0; i < n; ++i) {
-        *std::lower_bound(dp.begin(), dp.end(), seq[i], comp) = seq[i];
-      }
-      return std::distance(
-          dp.begin(),
-          std::lower_bound(dp.begin(), dp.end(), inf));
-    }
-  private:
-    size_t const n;
-    std::vector<T> const seq;
-    std::vector<T> dp;
-};
+template <typename T>
+size_t longest_incr_seq(std::vector<T> const &seq, T infty) {
+  size_t const n = s.size();
+  std::vector<T> dp(n, infty);
+  for (size_t i = 0; i < n; ++i) {
+    *std::lower_bound(begin(dp), end(dp), seq[i]) = seq[i];
+  }
+  return std::distance(
+      begin(dp),
+      std::lower_bound(begin(dp), end(dp), infty));
+}
+
+template <typename T, typename FnT>
+size_t longest_incr_seq(std::vector<T> const &seq, T infty, FnT comp) {
+  size_t const n = s.size();
+  std::vector<T> dp(n, infty);
+  for (size_t i = 0; i < n; ++i) {
+    *std::lower_bound(begin(dp), end(dp), seq[i], comp) = seq[i];
+  }
+  return std::distance(
+      begin(dp),
+      std::lower_bound(begin(dp), end(dp), infty));
+}
