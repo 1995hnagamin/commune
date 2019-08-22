af0e87b8c2e7f829ffe6ecf5c9e4528c8aab111a
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-08-22 21:49:12 +0900

    Add extgcd() to gcd.cpp

--- math/gcd.cpp
+++ math/gcd.cpp
@@ -5,4 +5,16 @@ T gcd(T m, T n) {
   return (n == 0) ? m : gcd(n, m % n);
 }
 
+template <typename T>
+T extgcd(T a, T b, T &x, T &y) {
+  T d = a;
+  if (b != 0) {
+    d = extgcd(b, a % b, y, x);
+    y -= (a / b) * x;
+  } else {
+    x = 1, y = 0;
+  }
+  return d;
+}
+
 }
