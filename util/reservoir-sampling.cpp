c83772bea14ac3a1eaf1ec751cfb2b96e350d886
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-08-25 01:06:16 +0900

    Rename parameters: s/begin/first/, s/end/last/

--- util/reservoir-sampling.cpp
+++ util/reservoir-sampling.cpp
@@ -1,10 +1,10 @@
 template<typename T, typename Iterator>
-std::vector<T> reservoir_sampling(Iterator begin, Iterator end, int k) {
+std::vector<T> reservoir_sampling(Iterator first, Iterator last, int k) {
   std::vector<T> ret(k);
-  Iterator iter = begin; int n;
+  Iterator iter = first; int n;
   for (n = 0; n < k; ++n, ++iter) { ret[n] = *iter; }
   std::random_device rd; std::mt19937 g(rd());
-  for (; iter != end; ++iter, ++n) {
+  for (; iter != last; ++iter, ++n) {
     std::uniform_int_distribution<> dis(0, n-1);
     auto const r = dis(g);
     if (r < k) {
