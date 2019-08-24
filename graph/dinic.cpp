1c5d8e471dbd881da5f23086c790663229649b59
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-08-25 04:11:22 +0900

    Convert dinic, ford_fulkerson to class templates

--- graph/dinic.cpp
+++ graph/dinic.cpp
@@ -1,15 +1,16 @@
+template <typename T>
 class dinic {
   public:
     struct edge {
-      explicit edge(size_t t, int c, size_t r):
+      explicit edge(size_t t, T c, size_t r):
         to(t), cap(c), rev(r) {}
-      size_t to; int cap; size_t rev;
+      size_t to; T cap; size_t rev;
     };
 
     explicit dinic(size_t v): G(v), level(v) {}
     ~dinic() = default;
 
-    void add_edge(size_t from, size_t to, int cap) {
+    void add_edge(size_t from, size_t to, T cap) {
       G[from].emplace_back(to, cap, G[to].size());
       G[to].emplace_back(from, 0, G[from].size() - 1);
     }
@@ -28,11 +29,11 @@ class dinic {
         }
       }
     }
-    int dfs(size_t v, size_t t, int f) {
+    T dfs(size_t v, size_t t, T f) {
       if (v == t) return f;
       for (auto &&e : G[v]) {
         if (e.cap <= 0 || level[v] >= level[e.to]) continue;
-        int d = dfs(e.to, t, std::min(f, e.cap));
+        T d = dfs(e.to, t, std::min(f, e.cap));
         if (d > 0) {
           e.cap -= d;
           reverse(e).cap += d;
@@ -41,20 +42,20 @@ class dinic {
       }
       return 0;
     }
-    int max_flow(size_t s, size_t t) {
-      int flow = 0;
+    T max_flow(size_t s, size_t t) {
+      T flow = 0;
       while (true) {
         bfs(s);
         if (level[t] < 0) return flow;
-        int f;
-        while ((f = dfs(s, t, INF)) > 0) {
+        T f;
+        while ((f = dfs(s, t, infty<T>)) > 0) {
           flow += f;
         }
       }
     }
   private:
     std::vector<std::vector<edge>> G;
-    std::vector<int> level;
+    std::vector<T> level;
     edge &reverse(edge const &e) {
       return G[e.to][e.rev];
     }
--- graph/ford-fulkerson.cpp
+++ graph/ford-fulkerson.cpp
@@ -1,23 +1,24 @@
+template <typename T>
 class ford_fulkerson {
   public:
     struct edge {
-      explicit edge(size_t t, int c, size_t r) : to(t), cap(c), rev(r) {}
-      size_t to; int cap; size_t rev;
+      explicit edge(size_t t, T c, size_t r) : to(t), cap(c), rev(r) {}
+      size_t to; T cap; size_t rev;
     };
 
     explicit ford_fulkerson(size_t v): G(v), used(v) {}
     ~ford_fulkerson() = default;
 
-    void add_edge(size_t from, size_t to, int cap) {
+    void add_edge(size_t from, size_t to, T cap) {
       G[from].emplace_back(to, cap, G[to].size());
       G[to].emplace_back(from, 0, G[from].size() - 1);
     }
-    int dfs(size_t v, size_t t, int f) {
+    T dfs(size_t v, size_t t, T f) {
       if (v == t) return f;
       used[v] = true;
       for (auto &&e : G[v]) {
         if (used[e.to] || e.cap <= 0) continue;
-        int d = dfs(e.to, t, std::min(f, e.cap));
+        T d = dfs(e.to, t, std::min(f, e.cap));
         if (d > 0) {
           e.cap -= d;
           reverse(e).cap += d;
@@ -26,11 +27,11 @@ class ford_fulkerson {
       }
       return 0;
     }
-    int max_flow(size_t s, size_t t) {
-      int flow = 0;
+    T max_flow(size_t s, size_t t) {
+      T flow = 0;
       while (true) {
         std::fill(used.begin(), used.end(), 0);
-        int f = dfs(s, t, INF);
+        T f = dfs(s, t, infty<T>);
         if (f == 0) return flow;
         flow += f;
       }
