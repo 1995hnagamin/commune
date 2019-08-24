c140af912afc13565ebf9d1db56da5b7ea188ce2
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-08-25 01:41:43 +0900

    Add support for tuple stringification

--- util/stringify.cpp
+++ util/stringify.cpp
@@ -16,6 +16,27 @@ void stringify_helper(std::ostringstream &oss, std::string const &s) {
   oss << "\"" << s << "\"";
 }
 
+template<typename ...T>
+struct stringify_tuple {
+  using tuple_t = std::tuple<T...>;
+  template <size_t I>
+  static int call_for(std::ostringstream &oss, tuple_t const &t) {
+    oss << (I > 0? ", ": "") << std::get<I>(t);
+    return 0;
+  }
+  template <size_t ...I>
+  static void call(std::ostringstream &oss, tuple_t const &t, std::index_sequence<I...>) {
+    oss << "(";
+    int arr[] = { 0, call_for<I>(oss, t)... }; (void)arr;
+    oss << ")";
+  }
+};
+
+template<typename ...T>
+void stringify_helper(std::ostringstream &oss, std::tuple<T...> const &t) {
+  stringify_tuple<T...>::call(oss, t, std::make_index_sequence<sizeof...(T)>{});
+}
+
 template<typename T, typename U>
 __attribute__((unused))
 void stringify_helper(std::ostringstream &oss, std::pair<T, U> const &p) {
