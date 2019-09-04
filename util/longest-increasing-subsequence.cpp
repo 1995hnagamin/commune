879f937834e98cce70cbfbda5e42a1da935e467a
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-09-05 04:22:34 +0900

    Fix typo in longest-increasing-subsequence.cpp

--- util/longest-increasing-subsequence.cpp
+++ util/longest-increasing-subsequence.cpp
@@ -1,6 +1,6 @@
 template <typename T>
 size_t longest_incr_subseq(std::vector<T> const &seq, T infty) {
-  size_t const n = s.size();
+  size_t const n = seq.size();
   std::vector<T> dp(n, infty);
   for (size_t i = 0; i < n; ++i) {
     *std::lower_bound(begin(dp), end(dp), seq[i]) = seq[i];
@@ -12,7 +12,7 @@ size_t longest_incr_subseq(std::vector<T> const &seq, T infty) {
 
 template <typename T, typename FnT>
 size_t longest_incr_subseq(std::vector<T> const &seq, T infty, FnT comp) {
-  size_t const n = s.size();
+  size_t const n = seq.size();
   std::vector<T> dp(n, infty);
   for (size_t i = 0; i < n; ++i) {
     *std::lower_bound(begin(dp), end(dp), seq[i], comp) = seq[i];
