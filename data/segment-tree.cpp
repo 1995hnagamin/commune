6e5191bdabee69762ccdd0c64e34d51b24b818a2
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2019-09-15 22:41:30 +0900

    Make sure to init all elems in segment_tree::dat
    Fix data/segment_tree.cpp.

--- data/segment-tree.cpp
+++ data/segment-tree.cpp
@@ -7,7 +7,7 @@ class segment_tree {
       segment_tree(app, un, std::vector<T>(size, un)) {
     }
     explicit segment_tree(FnT const &app, T const &un, std::vector<T> const &v):
-      append(app), unit(un), N(1 << log2(v.size())), dat(2*N-1) {
+      append(app), unit(un), N(1 << log2(v.size())), dat(2*N-1, un) {
       std::copy(begin(v), end(v), begin(dat)+N-1);
       for (size_t k = N - 1; k > 0; --k) {
         dat[k-1] = append(dat[2*k-1], dat[2*k]);
